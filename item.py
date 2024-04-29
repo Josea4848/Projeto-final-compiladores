4eaf1bff434939935a52b559b5ef57efca940ed8
José Alves <josealves3416@gmail.com>
2024-04-28 22:12:54 -0300

    item class

---
+++ item.py
@@ -0,0 +1,10 @@
+class Item:
+  def __init__(self, word, type):
+    self.word = word
+    self.type = type
+
+  def getWord(self):
+    return self.word
+  
+  def getType(self):
+    return self.type
